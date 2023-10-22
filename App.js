56077bd445d327cc942d9ca522c8ed5849cc65e6
Luisa Ihle <luisa.ihle@gmail.com>
2023-10-22 20:45:31 +0200

    create firebase 5.3

--- App.js
+++ App.js
@@ -1,3 +1,15 @@
+//import components
+import Start from "./components/Start";
+import Chat from "./components/Chat";
+
+// import react Navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+// create navigator
+const stack = createNativeStackNavigator();
+
+//import other functions
 import { StatusBar } from "expo-status-bar";
 import { useEffect, useState } from "react";
 import {
@@ -7,22 +19,40 @@ import {
   TouchableOpacity,
   View,
 } from "react-native";
-import Start from "./components/Start";
-import Chat from "./components/Chat";
 
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+//import Firestore
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
 
-// create navigator
-const stack = createNativeStackNavigator();
+// Firebase configuration object
+const App = () => {
+  const firebaseConfig = {
+    apiKey: "AIzaSyBFpzV36z1cz3h0NjyAj3AchHtSxheM6CM",
+    authDomain: "chatapp-b23ab.firebaseapp.com",
+    projectId: "chatapp-b23ab",
+    storageBucket: "chatapp-b23ab.appspot.com",
+    messagingSenderId: "232082015281",
+    appId: "1:232082015281:web:075de60af6adfb48f74110",
+    measurementId: "G-H8Y852PNWW",
+  };
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <stack.Navigator initialRouteName="Start">
-        <stack.Screen name="Start" component={Start} />
-        <stack.Screen name="Chat" component={Chat} />
-      </stack.Navigator>
-    </NavigationContainer>
-  );
-}
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+
+    return (
+      <NavigationContainer>
+        <stack.Navigator initialRouteName="Start">
+          <stack.Screen name="Start" component={Start} />
+          <stack.Screen name="Chat" component={Chat} />
+        </stack.Navigator>
+      </NavigationContainer>
+    );
+  }
+
+  export default App
