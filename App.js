be807ead96f773d56a0e8422463a96f260b1dc13
Luisa Ihle <luisa.ihle@gmail.com>
2023-10-23 19:46:04 +0200

    update app.js

--- App.js
+++ App.js
@@ -18,7 +18,6 @@ import {
 
 //import Firestore
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 //import the screens
@@ -39,7 +38,6 @@ const App = () => {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 
 // Initialize Cloud Firestore and get a reference to the service
 const db = getFirestore(app);
