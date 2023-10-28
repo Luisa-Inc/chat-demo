54a2a8a6f269a32235c8eb2a5fb7bc3c2ecb5aab
Luisa Ihle <luisa.ihle@gmail.com>
2023-10-28 12:57:08 +0200

    update start.js

--- components/Start.js
+++ components/Start.js
@@ -12,7 +12,7 @@ import {
 } from "react-native";
 import { getAuth, signInAnonymously } from "firebase/auth";
 
-const image = require("../images/Bg-Image.png");
+const image = require("../assets/BackgroundImage.png");
 
 const backgroundColors = {
   a: "#090C08",
